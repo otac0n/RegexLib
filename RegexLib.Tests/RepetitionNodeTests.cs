d6d4d5950dcdab66feb255decea8cc4cb26d24a6
John Gietzen <otac0n@gietzen.us>
2012-02-26 09:52:01 -0800

    Added `RepetitionNode` class.

---
+++ RegexLib.Tests/RepetitionNodeTests.cs
@@ -0,0 +1,291 @@
+﻿//-----------------------------------------------------------------------
+// <copyright file="RepetitionNodeTests.cs" company="(none)">
+//  Copyright © 2012 John Gietzen.
+//
+//  Permission is hereby granted, free of charge, to any person obtaining a copy
+//  of this software and associated documentation files (the "Software"), to deal
+//  in the Software without restriction, including without limitation the rights
+//  to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+//  copies of the Software, and to permit persons to whom the Software is
+//  furnished to do so, subject to the following conditions:
+//
+//  The above copyright notice and this permission notice shall be included in
+//  all copies or substantial portions of the Software.
+//
+//  THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+//  IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+//  FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+//  AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+//  LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+//  OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+//  THE SOFTWARE.
+// </copyright>
+// <author>John Gietzen</author>
+//-----------------------------------------------------------------------
+
+namespace RegexLib.Tests
+{
+    using System;
+    using System.Linq;
+    using NUnit.Framework;
+
+    [TestFixture]
+    public class RepetitionNodeTests
+    {
+        [Test]
+        public void ctor_WithNullRepeatedNode_ThrowsArgumentNullException()
+        {
+            Assert.That(() => new RepetitionNode(null, 0, null, true), Throws.InstanceOf<ArgumentNullException>());
+        }
+
+        [Test]
+        public void ctor_WithNegativeMinimum_ThrowsArgumentOutOfRangeException()
+        {
+            Assert.That(() => new RepetitionNode(new StubRegexNode(), -1, null, true), Throws.InstanceOf<ArgumentOutOfRangeException>());
+        }
+
+        [Test]
+        public void ctor_WithMaximumLessThanMinimum_ThrowsArgumentOutOfRangeException()
+        {
+            Assert.That(() => new RepetitionNode(new StubRegexNode(), 1, 0, true), Throws.InstanceOf<ArgumentOutOfRangeException>());
+        }
+
+        [Test]
+        public void Equals_WithNullReference_ReturnsFalse()
+        {
+            var subject = new RepetitionNode(new StubRegexNode(), 0, null, true);
+
+            Assert.That(subject.Equals(null), Is.False);
+        }
+
+        [Test]
+        public void Equals_WithOtherObject_ReturnsFalse()
+        {
+            var subject = new RepetitionNode(new StubRegexNode(), 0, null, true);
+            var other = new object();
+
+            Assert.That(subject.Equals(other), Is.False);
+        }
+
+        [Theory]
+        public void Equals_WithOtherRepetition_MatchesOnlyIfAllComponentsMatch(bool returnEquals, bool minMatch, bool maxMatch, bool eagerMatch)
+        {
+            var subject = new RepetitionNode(new StubRegexNode(returnEquals), 0, 1, true);
+            var other = new RepetitionNode(new StubRegexNode(), minMatch ? 0 : 1, maxMatch ? 1 : (int?)null, eagerMatch);
+
+            var expected = (returnEquals && minMatch && maxMatch && eagerMatch);
+
+            Assert.That(subject.Equals(other), Is.EqualTo(expected));
+        }
+
+        [Test]
+        public void Matches_WithEagerMatching_BacktracksToFewerMatches()
+        {
+            var subject = new RepetitionNode(new CharacterNode('a'), 0, null, true);
+
+            var actual = subject.GetMatches("aaa", 0).ToArray();
+
+            var matches = new[]
+            {
+                new RegexMatch("aaa", 0, 1),
+                new RegexMatch("aaa", 1, 1),
+                new RegexMatch("aaa", 2, 1),
+            };
+
+            var expected = new[]
+            {
+                new RegexMatch("aaa", 0, 3, matches.Take(3)),
+                new RegexMatch("aaa", 0, 2, matches.Take(2)),
+                new RegexMatch("aaa", 0, 1, matches.Take(1)),
+                new RegexMatch("aaa", 0, 0, matches.Take(0)),
+            };
+
+            Assert.That(actual, Is.EquivalentTo(expected));
+        }
+
+        [Test]
+        public void Matches_WithLazyMatching_BacktracksToMoreMatches()
+        {
+            var subject = new RepetitionNode(new CharacterNode('a'), 0, null, false);
+
+            var actual = subject.GetMatches("aaa", 0).ToArray();
+
+            var matches = new[]
+            {
+                new RegexMatch("aaa", 0, 1),
+                new RegexMatch("aaa", 1, 1),
+                new RegexMatch("aaa", 2, 1),
+            };
+
+            var expected = new[]
+            {
+                new RegexMatch("aaa", 0, 0, matches.Take(0)),
+                new RegexMatch("aaa", 0, 1, matches.Take(1)),
+                new RegexMatch("aaa", 0, 2, matches.Take(2)),
+                new RegexMatch("aaa", 0, 3, matches.Take(3)),
+            };
+
+            Assert.That(actual, Is.EquivalentTo(expected));
+        }
+
+        [Test]
+        public void Matches_EagerWithMinimum_YieldsAtLeastTheMinimum()
+        {
+            var subject = new RepetitionNode(new CharacterNode('a'), 2, null, true);
+
+            var actual = subject.GetMatches("aaa", 0).ToArray();
+
+            var matches = new[]
+            {
+                new RegexMatch("aaa", 0, 1),
+                new RegexMatch("aaa", 1, 1),
+                new RegexMatch("aaa", 2, 1),
+            };
+
+            var expected = new[]
+            {
+                new RegexMatch("aaa", 0, 3, matches.Take(3)),
+                new RegexMatch("aaa", 0, 2, matches.Take(2)),
+            };
+
+            Assert.That(actual, Is.EquivalentTo(expected));
+        }
+
+        [Test]
+        public void Matches_LazyWithMinimum_YieldsAtLeastTheMinimum()
+        {
+            var subject = new RepetitionNode(new CharacterNode('a'), 2, null, false);
+
+            var actual = subject.GetMatches("aaa", 0).ToArray();
+
+            var matches = new[]
+            {
+                new RegexMatch("aaa", 0, 1),
+                new RegexMatch("aaa", 1, 1),
+                new RegexMatch("aaa", 2, 1),
+            };
+
+            var expected = new[]
+            {
+                new RegexMatch("aaa", 0, 2, matches.Take(2)),
+                new RegexMatch("aaa", 0, 3, matches.Take(3)),
+            };
+
+            Assert.That(actual, Is.EquivalentTo(expected));
+        }
+
+        [Test]
+        public void Matches_EagerWithMinimumMoreThanAvailable_YieldsNone()
+        {
+            var subject = new RepetitionNode(new CharacterNode('a'), 4, null, true);
+
+            var actual = subject.GetMatches("aaa", 0).ToArray();
+
+            Assert.That(actual, Is.Empty);
+        }
+
+        [Test]
+        public void Matches_LazyWithMinimumMoreThanAvailable_YieldsNone()
+        {
+            var subject = new RepetitionNode(new CharacterNode('a'), 4, null, false);
+
+            var actual = subject.GetMatches("aaa", 0).ToArray();
+
+            Assert.That(actual, Is.Empty);
+        }
+
+        [Test]
+        public void Matches_EagerWithMaximum_YieldsAtLeastTheMaximum()
+        {
+            var subject = new RepetitionNode(new CharacterNode('a'), 0, 2, true);
+
+            var actual = subject.GetMatches("aaa", 0).ToArray();
+
+            var matches = new[]
+            {
+                new RegexMatch("aaa", 0, 1),
+                new RegexMatch("aaa", 1, 1),
+                new RegexMatch("aaa", 2, 1),
+            };
+
+            var expected = new[]
+            {
+                new RegexMatch("aaa", 0, 2, matches.Take(2)),
+                new RegexMatch("aaa", 0, 1, matches.Take(1)),
+                new RegexMatch("aaa", 0, 0, matches.Take(0)),
+            };
+
+            Assert.That(actual, Is.EquivalentTo(expected));
+        }
+
+        [Test]
+        public void Matches_LazyWithMaximum_YieldsAtLeastTheMaximum()
+        {
+            var subject = new RepetitionNode(new CharacterNode('a'), 0, 2, false);
+
+            var actual = subject.GetMatches("aaa", 0).ToArray();
+
+            var matches = new[]
+            {
+                new RegexMatch("aaa", 0, 1),
+                new RegexMatch("aaa", 1, 1),
+                new RegexMatch("aaa", 2, 1),
+            };
+
+            var expected = new[]
+            {
+                new RegexMatch("aaa", 0, 0, matches.Take(0)),
+                new RegexMatch("aaa", 0, 1, matches.Take(1)),
+                new RegexMatch("aaa", 0, 2, matches.Take(2)),
+            };
+
+            Assert.That(actual, Is.EquivalentTo(expected));
+        }
+
+        [Test]
+        public void Matches_EagerWithMinimumAndMaximum_YieldsBetweenTheMinimumAndMaximum()
+        {
+            var subject = new RepetitionNode(new CharacterNode('a'), 1, 2, true);
+
+            var actual = subject.GetMatches("aaa", 0).ToArray();
+
+            var matches = new[]
+            {
+                new RegexMatch("aaa", 0, 1),
+                new RegexMatch("aaa", 1, 1),
+                new RegexMatch("aaa", 2, 1),
+            };
+
+            var expected = new[]
+            {
+                new RegexMatch("aaa", 0, 2, matches.Take(2)),
+                new RegexMatch("aaa", 0, 1, matches.Take(1)),
+            };
+
+            Assert.That(actual, Is.EquivalentTo(expected));
+        }
+
+        [Test]
+        public void Matches_LazyWithMinimumAndMaximum_YieldsBetweenTheMinimumAndMaximum()
+        {
+            var subject = new RepetitionNode(new CharacterNode('a'), 1, 2, false);
+
+            var actual = subject.GetMatches("aaa", 0).ToArray();
+
+            var matches = new[]
+            {
+                new RegexMatch("aaa", 0, 1),
+                new RegexMatch("aaa", 1, 1),
+                new RegexMatch("aaa", 2, 1),
+            };
+
+            var expected = new[]
+            {
+                new RegexMatch("aaa", 0, 1, matches.Take(1)),
+                new RegexMatch("aaa", 0, 2, matches.Take(2)),
+            };
+
+            Assert.That(actual, Is.EquivalentTo(expected));
+        }
+    }
+}
---
+++ RegexLib/RepetitionNode.cs
@@ -0,0 +1,136 @@
+﻿//-----------------------------------------------------------------------
+// <copyright file="RepetitionNode.cs" company="(none)">
+//  Copyright © 2012 John Gietzen.
+//
+//  Permission is hereby granted, free of charge, to any person obtaining a copy
+//  of this software and associated documentation files (the "Software"), to deal
+//  in the Software without restriction, including without limitation the rights
+//  to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+//  copies of the Software, and to permit persons to whom the Software is
+//  furnished to do so, subject to the following conditions:
+//
+//  The above copyright notice and this permission notice shall be included in
+//  all copies or substantial portions of the Software.
+//
+//  THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+//  IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+//  FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+//  AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+//  LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+//  OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+//  THE SOFTWARE.
+// </copyright>
+// <author>John Gietzen</author>
+//-----------------------------------------------------------------------
+
+namespace RegexLib
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+
+    public class RepetitionNode : RegexNode
+    {
+        private readonly RegexNode repeated;
+        private readonly int min;
+        private readonly int? max;
+        private readonly bool eager;
+
+        public RepetitionNode(RegexNode repeated, int min, int? max, bool eager)
+        {
+            if (repeated == null)
+            {
+                throw new ArgumentNullException("other");
+            }
+
+            if (min < 0)
+            {
+                throw new ArgumentOutOfRangeException("min");
+            }
+
+            if (max.HasValue && max < min)
+            {
+                throw new ArgumentOutOfRangeException("max");
+            }
+
+            this.repeated = repeated;
+            this.min = min;
+            this.max = max;
+            this.eager = eager;
+        }
+
+        public override bool Equals(RegexNode other)
+        {
+            var repetition = other as RepetitionNode;
+            return !object.ReferenceEquals(repetition, null) &&
+                this.repeated == repetition.repeated &&
+                this.min == repetition.min &&
+                this.max == repetition.max &&
+                this.eager == repetition.eager;
+        }
+
+        public override int GetHashCode()
+        {
+            int hash = 0x51ED270B;
+            hash = (hash * -0x25555529) + this.repeated.GetHashCode();
+            hash = (hash * -0x25555529) + this.min.GetHashCode();
+            hash = (hash * -0x25555529) + this.max.GetHashCode();
+            hash = (hash * -0x25555529) + this.eager.GetHashCode();
+
+            return hash;
+        }
+
+        protected override IEnumerable<RegexMatch> GetMatchesImpl(string subject, int index)
+        {
+            foreach (var blah in this.Foo(0, subject, index, index, new List<RegexMatch>()))
+            {
+                yield return blah;
+            }
+        }
+
+        private IEnumerable<RegexMatch> Foo(int rep, string subject, int originalIndex, int index, List<RegexMatch> subMatches)
+        {
+            if (this.max != null && rep > this.max)
+            {
+                yield break;
+            }
+
+            if (!this.eager && WithinMinMax(rep))
+            {
+                var match = ComposeMatch(subject, originalIndex, subMatches);
+                yield return match;
+            }
+
+            foreach (var m in this.repeated.GetMatches(subject, index))
+            {
+                subMatches.Add(m);
+
+                foreach (var composite in this.Foo(rep + 1, subject, originalIndex, index + m.Length, subMatches))
+                {
+                    yield return composite;
+                }
+
+                subMatches.RemoveAt(subMatches.Count - 1);
+            }
+
+            if (this.eager && WithinMinMax(rep))
+            {
+                var match = ComposeMatch(subject, originalIndex, subMatches);
+                yield return match;
+            }
+
+            yield break;
+        }
+
+        private bool WithinMinMax(int rep)
+        {
+            return rep >= this.min && (this.max == null || rep <= this.max);
+        }
+
+        private RegexMatch ComposeMatch(string subject, int originalIndex, List<RegexMatch> subMatches)
+        {
+            var length = subMatches.Sum(m => m.Length);
+            return new RegexMatch(subject, originalIndex, length, subMatches);
+        }
+    }
+}
