1a2bdb492b789ac7aaed6f5339021e1ad10fcf1e
John Gietzen <otac0n@gietzen.us>
2012-03-04 20:33:17 -0800

    Added and improved JavaScript tests.

--- RegexLib.Tests/RegexParserTests.cs
+++ RegexLib.Tests/RegexParserTests.cs
@@ -51,7 +51,7 @@ namespace RegexLib.Tests
         public void Parse_WithCharacterRangeStartingWithCharacterClass_ThrowsFormatException()
         {
             Assert.That(
-                () => RegexParser.Parse(@"^[\w-c]$", RegexFlavor.JavaScript, RegexOptions.None),
+                () => RegexParser.Parse(@"[\w-c]", RegexFlavor.JavaScript, RegexOptions.None),
                 Throws.InstanceOf<FormatException>());
         }
 
@@ -59,7 +59,7 @@ namespace RegexLib.Tests
         public void Parse_WithCharacterRangeEndingWithCharacterClass_ThrowsFormatException()
         {
             Assert.That(
-                () => RegexParser.Parse(@"^[a-/w]$", RegexFlavor.JavaScript, RegexOptions.None),
+                () => RegexParser.Parse(@"[a-\w]", RegexFlavor.JavaScript, RegexOptions.None),
                 Throws.InstanceOf<FormatException>());
         }
 
@@ -67,8 +67,210 @@ namespace RegexLib.Tests
         public void Parse_WithCharacterClassWithOutOfOrderCharacters_ThrowsFormatException()
         {
             Assert.That(
-                () => RegexParser.Parse(@"^[z-a]$", RegexFlavor.JavaScript, RegexOptions.None),
+                () => RegexParser.Parse(@"[z-a]", RegexFlavor.JavaScript, RegexOptions.None),
                 Throws.InstanceOf<FormatException>());
+        }
+
+        [Test]
+        public void Parse_WithCharacterWithRange_ReturnsTheRange()
+        {
+            var actual = RegexParser.Parse(@"[a-z]", RegexFlavor.JavaScript, RegexOptions.None);
+
+            var expected = new CharacterClassNode('a', 'z');
+
+            Assert.That(actual, Is.EqualTo(expected));
+        }
+
+        [Test]
+        public void Parse_WithStarRepetition_ReturnsRepetition()
+        {
+            var actual = RegexParser.Parse(@"a*", RegexFlavor.JavaScript, RegexOptions.None);
+
+            var expected =
+                new RepetitionNode(
+                    new CharacterClassNode('a'),
+                    0,
+                    null,
+                    true);
+
+            Assert.That(actual, Is.EqualTo(expected));
+        }
+
+        [Test]
+        public void Parse_WithLazyStarRepetition_ReturnsRepetition()
+        {
+            var actual = RegexParser.Parse(@"a*?", RegexFlavor.JavaScript, RegexOptions.None);
+
+            var expected =
+                new RepetitionNode(
+                    new CharacterClassNode('a'),
+                    0,
+                    null,
+                    false);
+
+            Assert.That(actual, Is.EqualTo(expected));
+        }
+
+        [Test]
+        public void Parse_WithPlusRepetition_ReturnsRepetition()
+        {
+            var actual = RegexParser.Parse(@"a+", RegexFlavor.JavaScript, RegexOptions.None);
+
+            var expected =
+                new RepetitionNode(
+                    new CharacterClassNode('a'),
+                    1,
+                    null,
+                    true);
+
+            Assert.That(actual, Is.EqualTo(expected));
+        }
+
+        [Test]
+        public void Parse_WithLazyPlusRepetition_ReturnsRepetition()
+        {
+            var actual = RegexParser.Parse(@"a+?", RegexFlavor.JavaScript, RegexOptions.None);
+
+            var expected =
+                new RepetitionNode(
+                    new CharacterClassNode('a'),
+                    1,
+                    null,
+                    false);
+
+            Assert.That(actual, Is.EqualTo(expected));
+        }
+
+        [Test]
+        public void Parse_WithOptionalModifier_ReturnsRepetition()
+        {
+            var actual = RegexParser.Parse(@"a?", RegexFlavor.JavaScript, RegexOptions.None);
+
+            var expected =
+                new RepetitionNode(
+                    new CharacterClassNode('a'),
+                    0,
+                    1,
+                    true);
+
+            Assert.That(actual, Is.EqualTo(expected));
+        }
+
+        [Test]
+        public void Parse_WithLazyOptionalModifier_ReturnsRepetition()
+        {
+            var actual = RegexParser.Parse(@"a??", RegexFlavor.JavaScript, RegexOptions.None);
+
+            var expected =
+                new RepetitionNode(
+                    new CharacterClassNode('a'),
+                    0,
+                    1,
+                    false);
+
+            Assert.That(actual, Is.EqualTo(expected));
+        }
+
+        [Test]
+        public void Parse_WithRepititionIndicatorWithSingleNumber_ReturnsRepetition()
+        {
+            var actual = RegexParser.Parse(@"a{2}", RegexFlavor.JavaScript, RegexOptions.None);
+
+            var expected =
+                new RepetitionNode(
+                    new CharacterClassNode('a'),
+                    2,
+                    2,
+                    true);
+
+            Assert.That(actual, Is.EqualTo(expected));
+        }
+
+        [Test]
+        public void Parse_WithLazyRepititionIndicatorWithSingleNumber_ReturnsRepetition()
+        {
+            var actual = RegexParser.Parse(@"a{2}?", RegexFlavor.JavaScript, RegexOptions.None);
+
+            var expected =
+                new RepetitionNode(
+                    new CharacterClassNode('a'),
+                    2,
+                    2,
+                    false);
+
+            Assert.That(actual, Is.EqualTo(expected));
+        }
+
+        [Test]
+        public void Parse_WithRepititionIndicatorWithNoUpperLimit_ReturnsRepetition()
+        {
+            var actual = RegexParser.Parse(@"a{2,}", RegexFlavor.JavaScript, RegexOptions.None);
+
+            var expected =
+                new RepetitionNode(
+                    new CharacterClassNode('a'),
+                    2,
+                    null,
+                    true);
+
+            Assert.That(actual, Is.EqualTo(expected));
+        }
+
+        [Test]
+        public void Parse_WithLazyRepititionIndicatorWithNoUpperLimit_ReturnsRepetition()
+        {
+            var actual = RegexParser.Parse(@"a{2,}?", RegexFlavor.JavaScript, RegexOptions.None);
+
+            var expected =
+                new RepetitionNode(
+                    new CharacterClassNode('a'),
+                    2,
+                    null,
+                    false);
+
+            Assert.That(actual, Is.EqualTo(expected));
+        }
+
+        [Test]
+        public void Parse_WithRepititionIndicatorWithUpperLimit_ReturnsRepetition()
+        {
+            var actual = RegexParser.Parse(@"a{2,3}", RegexFlavor.JavaScript, RegexOptions.None);
+
+            var expected =
+                new RepetitionNode(
+                    new CharacterClassNode('a'),
+                    2,
+                    3,
+                    true);
+
+            Assert.That(actual, Is.EqualTo(expected));
+        }
+
+        [Test]
+        public void Parse_WithLazyRepititionIndicatorWithUpperLimit_ReturnsRepetition()
+        {
+            var actual = RegexParser.Parse(@"a{2,3}?", RegexFlavor.JavaScript, RegexOptions.None);
+
+            var expected =
+                new RepetitionNode(
+                    new CharacterClassNode('a'),
+                    2,
+                    3,
+                    false);
+
+            Assert.That(actual, Is.EqualTo(expected));
+        }
+
+        [Test]
+        public void Parse_WithRepititionIndicatorWithUpperLimitGreaterThanLowerLimit_ThrowsFormatException()
+        {
+            Assert.That(() => RegexParser.Parse(@"a{3,2}", RegexFlavor.JavaScript, RegexOptions.None), Throws.InstanceOf<FormatException>());
+        }
+
+        [Test]
+        public void Parse_WithLazyRepititionIndicatorWithUpperLimitGreaterThanLowerLimit_ThrowsFormatException()
+        {
+            Assert.That(() => RegexParser.Parse(@"a{3,2}?", RegexFlavor.JavaScript, RegexOptions.None), Throws.InstanceOf<FormatException>());
         }
 
         [Test]
@@ -109,13 +311,11 @@ namespace RegexLib.Tests
             var actual = RegexParser.Parse(@"\w", RegexFlavor.JavaScript, RegexOptions.None);
 
             var expected =
-                new AlternationNode(
-                    new CharacterClassNode('a', 'z'),
-                    new AlternationNode(
-                        new CharacterClassNode('A', 'Z'),
-                        new AlternationNode(
-                            new CharacterClassNode('0', '9'),
-                            new CharacterClassNode('_'))));
+                new CharacterClassNode(
+                    new CharacterClassNode.CharacterRange('a', 'z'),
+                    new CharacterClassNode.CharacterRange('A', 'Z'),
+                    new CharacterClassNode.CharacterRange('0', '9'),
+                    new CharacterClassNode.CharacterRange('_', '_'));
 
             Assert.That(actual, Is.EqualTo(expected));
         }
@@ -123,14 +323,12 @@ namespace RegexLib.Tests
         [Test]
         public void Parse_WithMultipleCharacters_YieldsConcatenation()
         {
-            var actual = RegexParser.Parse(@"abc", RegexFlavor.JavaScript, RegexOptions.None);
+            var actual = RegexParser.Parse(@"ab", RegexFlavor.JavaScript, RegexOptions.None);
 
             var expected =
                 new ConcatenationNode(
                     new CharacterClassNode('a'),
-                    new ConcatenationNode(
-                        new CharacterClassNode('b'),
-                        new CharacterClassNode('c')));
+                    new CharacterClassNode('b'));
 
             Assert.That(actual, Is.EqualTo(expected));
         }
@@ -138,14 +336,12 @@ namespace RegexLib.Tests
         [Test]
         public void Parse_WithAlternation_YieldsAlternation()
         {
-            var actual = RegexParser.Parse(@"a|ab", RegexFlavor.JavaScript, RegexOptions.None);
+            var actual = RegexParser.Parse(@"a|b", RegexFlavor.JavaScript, RegexOptions.None);
 
             var expected =
                 new AlternationNode(
                     new CharacterClassNode('a'),
-                    new ConcatenationNode(
-                        new CharacterClassNode('a'),
-                        new CharacterClassNode('b')));
+                    new CharacterClassNode('b'));
 
             Assert.That(actual, Is.EqualTo(expected));
         }
