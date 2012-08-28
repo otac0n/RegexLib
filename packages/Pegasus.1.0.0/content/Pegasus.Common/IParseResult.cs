f2cedd1f4f491dd28b6222507d353ba8a02ee2ba
John Gietzen <otac0n@gietzen.us>
2012-08-27 19:00:19 -0700

    Added the Pegasus parser generator package.

---
+++ packages/Pegasus.1.0.0/content/Pegasus.Common/IParseResult.cs
@@ -0,0 +1,32 @@
+// -----------------------------------------------------------------------
+// <copyright file="IParseResult{T}.cs" company="(none)">
+//   Copyright © 2012 John Gietzen.  All Rights Reserved.
+//   This source is subject to the MIT license.
+//   Please see license.txt for more information.
+// </copyright>
+// -----------------------------------------------------------------------
+
+namespace Pegasus.Common
+{
+    /// <summary>
+    /// Encapsulates the success or failure of a particular parsing operation along with the result of operation.
+    /// </summary>
+    /// <typeparam name="T">The type of the parsing operation's result.</typeparam>
+    public interface IParseResult<out T>
+    {
+        /// <summary>
+        /// Gets the ending cursor of the match.
+        /// </summary>
+        Cursor EndCursor { get; }
+
+        /// <summary>
+        /// Gets the starting cursor of the match.
+        /// </summary>
+        Cursor StartCursor { get; }
+
+        /// <summary>
+        /// Gets the resulting value of the parsing operation.
+        /// </summary>
+        T Value { get; }
+    }
+}
